be27bc470140b4bd684051966e6d6e0103bcea8e
LucioAlonso <lucioalonso530@gmail.com>
2022-12-04 01:49:13 -0300

    Se crea el elemento claim para mostrar datos del reclamo

--- src/components/Claim_History.js
+++ src/components/Claim_History.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, Navigate } from 'react-router-dom';
 import '../stylesheets/Claim_New.css';
 import useUser from '../hooks/useUser';
+import Claims from './Claims';
 
 const Claim_History = () => {
   
@@ -11,10 +12,32 @@ const Claim_History = () => {
   return (
     <>
     {
-      (userData != null)
+      (userData == null)
       ?
       (<>
         {/* ACA VA TODO EL SISTEMA PARA VER LOS RECLAMOS */}
+        <div className='contenedor'>
+          <div className='contenedor-login'>
+          <h1>Historial de Reclamos</h1>
+          <p className='text'>Tipo:</p>
+            <select  required className='container-input'>
+              <option selected value='todos'>Todos</option>
+              <option value='pluvial'>Pluvial</option>
+              <option value='arbolado'>Arbolado</option>
+              <option value='alumbrado'>Alumbrado</option>
+              <option value='limpieza'>Limpieza</option>
+            </select>
+
+          <p className='text'>Estado:</p>
+            <select  required className='container-input'>
+              <option selected value="Resueltos-SinResolver" disabled>Resueltos/Sin Resolver</option>
+              <option value='Resueltos'>Resueltos</option>
+              <option value='Sin Resolver'>Sin Resolver</option>
+            </select>
+              <Claims />
+              <Claims />
+          </div>
+        </div>
       </>)
       :
       (<Navigate to='/login' />)
--- src/components/Claims.js
+++ src/components/Claims.js
@@ -4,20 +4,22 @@ import '../stylesheets/Claims.css';
 import useUser from '../hooks/useUser';
 
 const Claims = () => {
-  
+  //le puedo pasar por parametros y que lo vaya creando la clase que lo solicita
   const {userData} = useUser();
 
   return (
-			<div>
-				<div>
+			<div className='contenedor-general-claim'>
+				<div className='contenedor-icono'>
 					<img 
 						className='icono-reclamos'
-						src={require('../images/reclamos-icono.png')}
+						src={require('../images/tipo-pluvial.png')}
 						alt = 'Icono de los reclamos'
 					/>
 				</div>
-				<div>
-						
+				<div className='info'>
+					<h4 className='contenedor-text-fecha'>Fecha: </h4>
+					<h4 className='contenedor-text-estado'>ESTADO</h4>
+					<h4 className='contenedor-text-direccion'>Dirección: </h4>
 				</div>
 			</div>
   );
